d89dc6c7206ce7b51a7b8da661f4fea2e84ced8a
Nav <navdeep_aulakh24@hotmail.com>
2023-03-27 15:06:53 -0400

    added a main page for layout

---
+++ client/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProjectContainer from './ProjectContainer';
+
+export default function Main() {
+    
+    return(
+        <section id="hero" class="d-flex align-items-center">
+          <div class="container d-flex flex-column align-items-center" data-aos="zoom-in" data-aos-delay="100">
+            <h1>Navdeep Aulakh</h1>
+            <h2>I'm a professional Full-Stack Developer</h2>
+            <a href="#about" class="btn-about">About Me</a>
+          </div>
+        </section>
+    )}
